27d4921e5a8422c1fded8605556c0cb3f32478ce
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-30 19:38:58 +0500

    fixed-width

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -52,7 +52,7 @@ function Products() {
     ));
 
   return (
-    <div className="w-full flex flex-col md:flex-row p-2 items-center md:items-start h-fit dark:bg-gray-800 gap-2">
+    <div className="w-full flex flex-col md:flex-row p-2 items-center md:items-start h-fit dark:bg-gray-800 gap-2 flex-wrap">
       {loading
         ? renderLoading()
         : error
