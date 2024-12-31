d2cd033eab44823527f70d5a8601865d0c9f0d59
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-31 15:52:38 +0500

    fixed loading

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -28,7 +28,7 @@ function Products() {
   }, []);
 
   const renderLoading = () => (
-    <div className="flex justify-center items-center">
+    <div className="flex w-full justify-center items-center">
     <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-pink-500 rounded-full animate-spin"></div>
 </div>
   );
