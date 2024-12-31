2ec483ae0429fb0953265fbc956176bcea5f42a1
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-31 19:41:42 +0500

    ....

--- src/pages/Darkmode.jsx
+++ src/pages/Darkmode.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { SunIcon, MoonIcon } from '@heroicons/react/solid';
 
 const DarkModeToggle = () => {
   const [darkMode, setDarkMode] = useState(
@@ -20,9 +21,15 @@ const DarkModeToggle = () => {
   return (
     <button
       onClick={toggleDarkMode}
-      className="px-4 py-2 text-white bg-gray-800 rounded-lg shadow-md dark:bg-gray-200 dark:text-black"
+      className="px-4 py-2 text-white bg-gray-800 rounded-lg shadow-md dark:bg-gray-200 dark:text-black flex items-center space-x-2"
     >
-      {darkMode ? 'Light Mode' : 'Dark Mode'}
+      {/* Conditionally render sun or moon icon */}
+      {darkMode ? (
+        <MoonIcon className="w-5 h-5" />
+      ) : (
+        <SunIcon className="w-5 h-5" />
+      )}
+      <span>{darkMode ? 'Light Mode' : 'Dark Mode'}</span>
     </button>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Products from './Products'
 import Feature from '../components/Feature'
 import Carousel from '../components/Carousel'
+import { HeroParallax } from '../components/ui/Hero-parrallax'
+import { fetchProducts } from '../api'
 
 function Home() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+  
+    const getProducts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetchProducts();
+        setProducts(response.data.data || []);
+  
+      } catch (err) {
+        setError('Error fetching products');
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    useEffect(() => {
+      getProducts();
+    }, []);
   return (
     <main class="dark:bg-gray-800 bg-white  relative overflow-x-h">
-    <div class="bg-white dark:bg-gray-800 flex relative z-20 items-center overflow-hidden">
-        <div class="container mx-auto px-6 flex relative">
-            <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
-                <span class="w-20 h-2 bg-gray-800 dark:bg-white mb-12">
-                </span>
-                <h1 class="font-bebas-neue uppercase text-5xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
-                    Get Your
-                    <span class="text-5xl sm:text-7xl">
-                    Timepiece Now                    
-                    </span>
-                </h1>
-                <p class="text-sm sm:text-base text-gray-700 dark:text-white">
-                    Dimension of reality that makes change possible and understandable. An indefinite and homogeneous environment in which natural events and human existence take place.
-                </p>
-                <div class="flex mt-8">
-                    <a href="/profile" class="uppercase py-2 px-4 rounded-lg bg-pink-500 border-2 border-transparent text-white sm:text-md text-sm mr-4 hover:bg-pink-400">
-                        Get started
-                    </a>
-                    <a href="/product" class="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-pink-500 text-pink-500 dark:text-white hover:bg-pink-500 hover:text-white sm:text-md text-sm">
-                        Products
-                    </a>
-                </div>
-                
-            </div>
-            <div class="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
-                <img src="https://www.tailwind-kit.com/images/object/10.png" class="max-w-xs md:max-w-sm m-auto hover:-translate-y-2 hover:transition-all"/>
-            </div>
-        </div>
-    </div>
+    <HeroParallax products={products}/>
 <div className='w-full flex items-center justify-center overflow-hidden'>
 <Carousel/>
 </div>
