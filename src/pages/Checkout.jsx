b7c585ccb2aa33129f40b08dc654b865fda46305
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-31 20:52:03 +0500

    ........

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -101,7 +101,7 @@ function Checkout() {
   };
 
   return (
-    <div className="max-w-4xl mt-2 mb-2 mx-auto p-6 bg-black shadow-lg">
+    <div className="max-w-4xl mt-2 mb-2 mx-auto p-6 bg-transparent shadow-lg">
       <h1 className="text-3xl font-bold text-pink-500 mb-6 border-b-2 border-pink-500"><span className='text-white'>Check</span>kout</h1>
       
       <div className="flex flex-col md:flex-row items-center mb-6">
