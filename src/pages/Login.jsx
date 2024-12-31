632775294f3174847c7234d7d0dfbcce8667efa3
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-31 17:28:49 +0500

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,14 +5,13 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);  // Track loading state
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const apiUrl = import.meta.env.VITE_API_URL;
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    setLoading(true);  // Start loading animation
+    setLoading(true);
 
     try {
       const response = await axios.post(`https://times-store-production.up.railway.app/api/auth/local`, {
@@ -21,61 +20,97 @@ const Login = () => {
       });
 
       const token = response.data.jwt;
-      localStorage.setItem('jwt', token);  
-      setLoading(false);  // Stop loading animation
+      localStorage.setItem('jwt', token);
+      setLoading(false);
       navigate('/');
     } catch (error) {
       console.error('Login error:', error);
       alert('Invalid Email Or Password');
-      setLoading(false);  // Stop loading animation
+      setLoading(false);
     }
   };
 
   return (
-    <div className="min-h-[85vh] dark:bg-gray-800 flex items-center justify-center p-3">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
-        <h2 className="text-2xl font-semibold text-center text-gray-700 mb-6">Login</h2>
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600 mb-2">Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-500"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-sm font-medium text-gray-600 mb-2">Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-500"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full py-3 bg-pink-600 text-white font-semibold rounded-md hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500"
-            disabled={loading}  // Disable button while loading
-          >
-            {loading ? (
-              <div className="flex justify-center">
-                <div className="animate-spin border-t-2 border-white w-6 h-6 rounded-full"></div>
-              </div>
-            ) : (
-              'Login'
-            )}
-          </button>
-        </form>
-        <div className="mt-4 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
-          <a href="/register" className="text-pink-600 hover:underline">
-            Register here
+    <div className="flex flex-wrap w-full">
+      <div className="flex flex-col w-full md:w-1/2">
+        <div className="flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24">
+          <a href="#" className="p-4 text-xl font-bold text-white bg-black">
+            ZENTIME
           </a>
         </div>
+        <div className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
+          <p className="text-3xl text-center">Welcome.</p>
+          <form className="flex flex-col pt-3 md:pt-8" onSubmit={handleLogin}>
+            <div className="flex flex-col pt-4">
+              <div className="flex relative">
+                <span className="inline-flex items-center px-3 border-t bg-white border-l border-b border-gray-300 text-gray-500 shadow-sm text-sm">
+                  <svg
+                    width="15"
+                    height="15"
+                    fill="currentColor"
+                    viewBox="0 0 1792 1792"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"></path>
+                  </svg>
+                </span>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col pt-4 mb-12">
+              <div className="flex relative">
+                <span className="inline-flex items-center px-3 border-t bg-white border-l border-b border-gray-300 text-gray-500 shadow-sm text-sm">
+                  <svg
+                    width="15"
+                    height="15"
+                    fill="currentColor"
+                    viewBox="0 0 1792 1792"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"></path>
+                  </svg>
+                </span>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-black shadow-md hover:text-black hover:bg-white focus:outline-none focus:ring-2"
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Submit'}
+            </button>
+          </form>
+          <div className="pt-12 pb-12 text-center">
+            <p>
+              Don&apos;t have an account?
+              <a href="/register" className="font-semibold underline">
+                Register here.
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 shadow-2xl">
+        <img
+          className="hidden object-cover w-full h-screen md:block"
+          src="https://www.tailwind-kit.com/images/object/9.jpg"
+          alt="Login visual"
+        />
       </div>
     </div>
   );
