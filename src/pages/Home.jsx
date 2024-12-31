6a39d51e51c5f14da9da61596646bcf0e12681c8
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-31 21:14:56 +0500

    .........

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Feature from '../components/Feature';
 import Carousel from '../components/Carousel';
 import { HeroParallax } from '../components/ui/Hero-parrallax';
 import { fetchProducts } from '../api';
+import { CanvasRevealEffectDemo3 } from '../components/CanvasRevealEffectDemo3';
 
 function Home() {
   const [products, setProducts] = useState([]);
@@ -27,16 +28,17 @@ function Home() {
 
   useEffect(() => {
     getProducts();
-  }, []); // Empty dependency array ensures it runs only on mount
+  }, []); 
 
   return (
     <main className="bg-black relative overflow-x-hidden">
-      {loading &&(<div className="flex w-full justify-center items-center">
+      {loading &&(<div className="flex w-full justify-center items-center mt-8">
     <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-pink-500 rounded-full animate-spin"></div>
 </div>)}
       {error && <p>{error}</p>}
       <HeroParallax products={items} />
-      <div className="w-full overflow-x-hidden flex items-center justify-center">
+      <div className="w-full overflow-x-hidden flex space-y-16 items-center justify-center">
+        <CanvasRevealEffectDemo3/>
         <Feature />
       </div>
     </main>
