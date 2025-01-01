bdf18f3918c6fb93cf593b59fe64efd50923587f
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-01-01 12:07:58 +0500

    added analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 
 function Layout() {
@@ -9,6 +10,7 @@ function Layout() {
     <div className='w-full flex min-h-screen flex-col bg-black overflow-x-hidden '>
         <Navigation/>          
         <Outlet/>
+        <Analytics/>
         <Footer/>
     </div>
   )
