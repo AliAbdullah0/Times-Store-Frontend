c187b1f39463b57ee558aa2d61eea382180a82b4
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-01-02 15:49:25 +0500

    ....

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,35 +1,39 @@
-import { useEffect, useState } from "react";
-import {ImagesSlider } from './ui/ImageSlider' // Import ImagesSlider component
+import { motion } from "framer-motion";
+import React from "react";
+import { ImagesSlider } from "./ui/ImageSlider";
 
-const HeroSection = () => {
-  const [imagesData, setImagesData] = useState([]);
-  const [loading, setLoading] = useState(true); // Track loading state
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await fetch("https://times-store-production.up.railway.app/api/sliderimages?populate=*");
-        const data = await response.json();
-        setImagesData(data.Images.map((item)=>item)); // Accessing images from the API response
-        setLoading(false); // Set loading to false when images are fetched
-      } catch (error) {
-        console.error("Error fetching images:", error);
-        setLoading(false); // Stop loading even if the fetch fails
-      }
-    };
-
-    fetchImages();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>; // Optionally display a loading state
-  }
-
+export function ImagesSliderDemo() {
+  const images = [
+    "https://www.watchesofswitzerland.com/medias/wosus-hp-tudor-desktop-jan25.jpg?context=bWFzdGVyfGltYWdlc3w5NjA1NDR8aW1hZ2UvanBlZ3xhRE5oTDJneVl5ODVNekk0TkRrMk16UTFNVEU0TDNkdmMzVnpMV2h3TFhSMVpHOXlMV1JsYzJ0MGIzQXRhbUZ1TWpVdWFuQm58ODIyNmNhZGEzMDY5ZWRhYjI4ZTNiZGQzOWIxYzYwNTE0NjgxMzBhYTk4ZWM0NzE2MTA4MTU2NmRhNmNhYmQxOA&imwidth=1920",
+    "https://www.watchesofswitzerland.com/medias/wosus-patek-philippe-desktop-carousel-may-23.jpg?context=bWFzdGVyfHJvb3R8MjIxNjAwfGltYWdlL2pwZWd8YURRNUwyZzBZaTg1TWpJd05URTFOalU1T0RBMkxtcHdad3w5M2U2MGJjZGQ2NDU5YjU3MGNmN2UyMmFiN2NiNmFhZTllMjczYjk1ZGQ1ZGM1NzIzNjMyZDMxN2NkNDU4MWQ3&imwidth=1920",
+    "https://www.watchesofswitzerland.com/medias/wosus-grand-seiko-carousel-desktop-dec23.jpg?context=bWFzdGVyfGltYWdlc3wyMzQ5ODh8aW1hZ2UvanBlZ3xhVzFoWjJWekwyZzNaUzlvTjJFdk9USTFNVFV4T0RZM056QXlNaTVxY0djfDk3NmU2ZWEzZWE1MTliZTAwZmQ1ZjlhMDZjNjg3MmExNGRhMjI0MTJiMTg0ODUwODcwMjY2NTAxODM3YTc5MDU&imwidth=1920",
+  ];
   return (
-    <div>
-      <ImagesSlider images={imagesData} autoplay={true} />
-    </div>
+    (<ImagesSlider className="h-[40rem]" images={images}>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -80,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className="z-50 flex flex-col justify-center items-center">
+        <motion.p
+          className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+          The hero section slideshow <br /> nobody asked for
+        </motion.p>
+        <button
+          className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+          <span>Join now →</span>
+          <div
+            className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+        </button>
+      </motion.div>
+    </ImagesSlider>)
   );
-};
-
-export default HeroSection;
+}
